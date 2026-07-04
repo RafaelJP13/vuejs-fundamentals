b5d57a8b001f38836db1088b1ad997e8827accaf
Rafael Fernandes <rafaelsf013@gmail.com>
2026-07-04 11:07:11 -0300

    feat: add routing and layout components for the application

---
+++ src/app/router/routes.ts
@@ -0,0 +1,12 @@
+import AppLayout from "@/shared/layouts/AppLayout.vue";
+import type { RouteRecordRaw } from "vue-router";
+
+export const routes: RouteRecordRaw[] = [
+
+    {
+        path: '/',
+        component: AppLayout,
+
+    }
+
+]
---
+++ src/shared/layouts/AppLayout.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex flex-col min-h-screen ">
+        <header class="h-16 border-b border-zinc-800">Header</header>
+        <div class="flex flex-1">
+            <aside class="w-64 bg-zinc-900 border-r border-zinc-800 p-4">Menu</aside>
+            <main class="p-4">Main</main>
+        </div>
+    </div>
+</template> 
