f4f44897637a6b2c5c66d53bfe1421b5157a4172
Rafael Fernandes <rafaelsf013@gmail.com>
2026-07-05 12:10:44 -0300

    feat: create Title component and update CounterPage and HomePage to use it

--- src/modules/samples/pages/CounterPage.vue
+++ src/modules/samples/pages/CounterPage.vue
@@ -1,5 +1,11 @@
+<script setup lang="ts">
+import Title from '@/shared/components/Title.vue';
+
+</script>
+
 <template>
-    <div>
-        <span>Contador</span>
+    <div class="flex flex-col gap-6">
+        <Title text="Contador" sub="Exemplo de Contador" />
+        <div>Conteúdo do Contador</div>
     </div>
 </template>  
--- src/modules/samples/pages/HomePage.vue
+++ src/modules/samples/pages/HomePage.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import Title from '@/shared/components/Title.vue';
+
+
+
+
+</script>
+
 <template>
-    <h1>Home Page</h1>
+    <Title text="Dashboard" sub="Bem-Vindo ao painel de controle"></Title>
 </template>
